3c8e8e21e0cfeb670d4981aa7d3e97dbfe5ed0f4
Ngọc Sáng <sang_nguyen@datahouse.com>
2025-08-23 17:20:14 +0700

    feat: init base snake game

---
+++ Main.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <cstdlib>
+#include <unistd.h>   // sleep, usleep
+#include <termios.h>  // getch
+#include <fcntl.h>    // kbhit
+using namespace std;
+
+void gotoxy(int column, int line) {
+    printf("\033[%d;%dH", line, column);
+}
+
+int getch() {
+    struct termios oldt, newt;
+    int ch;
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    ch = getchar();
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    return ch;
+}
+
+int kbhit() {
+    struct termios oldt, newt;
+    int ch;
+    int oldf;
+
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+    fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+
+    ch = getchar();
+
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    fcntl(STDIN_FILENO, F_SETFL, oldf);
+
+    if (ch != EOF) {
+        ungetc(ch, stdin);
+        return 1;
+    }
+    return 0;
+}
+
+struct Point {
+    int x, y;
+};
+
+class SNAKE {
+public:
+    Point A[100];
+    int snake_length;
+
+    SNAKE() {
+        snake_length = 5;
+        A[0].x = 10; A[0].y = 10;
+        A[1].x = 11; A[1].y = 10;
+        A[2].x = 12; A[2].y = 10;
+        A[3].x = 13; A[3].y = 10;
+        A[4].x = 14; A[4].y = 10;
+    }
+
+    void Draw() {
+        for (int i = 0; i < snake_length; i++) {
+            gotoxy(A[i].x, A[i].y);
+            cout << "X";
+        }
+    }
+
+    void Move(int direction) {
+        for (int i = snake_length - 1; i > 0; i--)
+            A[i] = A[i - 1];
+
+        if (direction == 0) A[0].x = A[0].x + 1; // phải
+        if (direction == 1) A[0].y = A[0].y + 1; // xuống
+        if (direction == 2) A[0].x = A[0].x - 1; // trái
+        if (direction == 3) A[0].y = A[0].y - 1; // lên
+    }
+};
+
+int main() {
+    SNAKE r;
+    int direction = 0;
+    char t;
+
+    system("clear");
+    while (1) {
+        if (kbhit()) {
+            t = getch();
+            if (t == 'a') direction = 2;
+            if (t == 'w') direction = 3;
+            if (t == 'd') direction = 0;
+            if (t == 's') direction = 1;
+            if (t == 'q') break;
+        }
+        system("clear");
+        r.Draw();
+        r.Move(direction);
+        usleep(200000); // 200ms
+    }
+    return 0;
+}
